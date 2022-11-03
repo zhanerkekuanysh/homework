215626e85064eab2a3cbbbba11cb66cd25248982
Куаныш Жанерке Амангельдыкызы <KuanyshZha@lcoast.itstep>
2022-11-03 16:58:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// let a = prompt() 
+// let b = prompt() 
+// function num(a, b) { 
+//     if (a > b || a < b) { 
+//         return 1 
+//     } else { 
+//         return 0 
+//     } 
+// } 
+// console.log(num(a, b));
+
+
+
+// function factorial(n) {
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+//   }
+   
+//   alert( factorial(8) ) 
+
+
+
+// function number(a,b,c) {
+//     let e = +(''+a+b+c)
+//     console.log(e)
+//     }
+//     number(1,2,3)
+
+
+
